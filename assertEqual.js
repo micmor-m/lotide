07fd5a8ad1093cff65dc282aaeaa50a534ab7708
micmor-m <micmor.m@gmail.com>
2020-07-21 01:27:37 +0000

    update assertEqual file

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected);
+  } 
+ 
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(false, true);
+assertEqual("Lighthouse", "lighthouse");
