b232d958dc5529279e2144d5a2f4c54becd5a93d
micmor-m <micmor.m@gmail.com>
2020-07-21 13:41:39 +0000

    fixed errors using eslint

--- head.js
+++ head.js
@@ -4,13 +4,12 @@ const assertEqual = function(actual, expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  } 
- 
+  }
 };
 
 const head = function(array) {
   return array[0];
-}
+};
 
 // TEST CODE
 assertEqual(head([5,6,7]), 5);
