e40389901764a7340ce9d20628ad117a8ac28874
micmor-m <micmor.m@gmail.com>
2020-07-21 02:22:07 +0000

    update tail.js file

---
+++ tail.js
@@ -0,0 +1,37 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  } 
+ 
+};
+
+const tail = function(array) {
+  const arrayTail = [];
+  
+  for (let i = 1; i < array.length; i++) {
+    arrayTail.push(array[i]);
+  }
+  console.log(arrayTail);
+  result = arrayTail; 
+  return result;
+};
+
+
+
+// Test Case: Check the original array 
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
+
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+/*
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(false, true);
+assertEqual("Lighthouse", "lighthouse");
+*/
