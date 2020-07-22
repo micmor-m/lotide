e1d20319f9d5af9b790f2cea147c74fe8be263fd
micmor-m <micmor.m@gmail.com>
2020-07-22 12:45:09 +0000

    correct error of eqArrays.js by eslint

--- eqArrays.js
+++ eqArrays.js
@@ -1,20 +1,20 @@
 /*
 Implement a function eqArrays which takes in two arrays
- and returns true or false, based on a perfect match. 
+ and returns true or false, based on a perfect match.
 */
 
 const eqArrays = function(array1, array2) {
- if (array1.length !== array2.length) {
-   return false;
- } else {
-   for (let i = 0; i < array1.length; i++) {
-     if (array1[i] !== array2[i]) {
-       return false;
-     }
-   }
- }
- return true;
-}
+  if (array1.length !== array2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
 
 //For test
 console.log(eqArrays([], [])); // => true
